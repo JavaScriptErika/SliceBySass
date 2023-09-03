7d4c29835d00bd9bf539a5e21ab6f89deb50e415
JavaScriptErika <erikajritter@gmail.com>
2023-09-03 10:59:04 -0400

    update types, add in acitivities, coordinates

--- src/components/cards/Card.tsx
+++ src/components/cards/Card.tsx
@@ -2,14 +2,13 @@ import React from 'react';
 import Col from 'react-bootstrap/Col';
 import { truncateDescription, truncateToThreeDecimalPlaces } from 'src/utils';
 import Image from 'react-bootstrap/Image';
-// import Logo from '../../assets/BirdTreeLogo.svg'
 
 interface CardProps {
     name: string,
-    img?: {url: string, altText: string},
+    img: {url: string, altText: string} | null,
     description: string,
-    coordinates: [string, string],
-    // activities: any,
+    coordinates: [{lat: string, long: string}],
+    activities: any,
     link: string;
 }
 
@@ -25,17 +24,22 @@ const formatCoordinates = (lat: string, long: string): string => {
     return `${Math.abs(truncatedLat)} ${latDirection} ${Math.abs(truncatedLong)} ${longDirection}`;
 }
 
-const Card = ({name, img, description, coordinates, link}: CardProps) => {
+const Card = ({name, img, description, coordinates, activities, link}: CardProps) => {
     // No more than 4 cards in each row
     return (
                 <Col xl={3} lg={4} md={6} className='rounded'>
+                    {/* Need a back up image */}
                     {img ? <Image src={img.url} alt={img.altText} className='img-fluid' /> : null}
                     <p>{name}</p>
-                    {/* <p>{activities[0]}</p> */}
-                    {/* <p>{img.credit}</p> */}
                     <p>{truncateDescription(description, 150)}</p>
-                    <p>{formatCoordinates(coordinates[0], coordinates[1])}</p>
-                    {/* <p>{activities}</p> */}
+                    <p>{formatCoordinates(coordinates[0].lat, coordinates[0].long)}</p>
+                    {activities && activities.length > 0 ? 
+                activities.map((activity: any) => {
+                    return (
+                        <p key={activity.id}>
+                            {activity.name}
+                        </p>
+                    )}) : null}   
                     <a href={link} target="_blank" rel='noopener noreferrer'>Go to Park Page</a>
                 </Col>
   
--- src/components/cards/Cards.tsx
+++ src/components/cards/Cards.tsx
@@ -5,11 +5,23 @@ import Card from './Card'
 import Spinner from 'react-bootstrap/Spinner';
 import { useGetNationalParks } from '../../hooks/useGetNationalParks';
 
+interface ParkItem {
+    id: string,
+    fullName: string,
+    description: string,
+    images: [{url: string, altText: string}] | null,
+    latitude: string,
+    longitude: string,
+    activities: [{ id: string, name: string}],
+    url: string,
+}
+
 const Cards = () => {
     const { data, isLoading, isError, error } = useGetNationalParks();
     const { data: parks } = data ?? {};
 
 console.log(error)
+// TODO -- make this better
     if (isLoading) {
         return <Spinner animation="border" />;
     }
@@ -22,7 +34,7 @@ console.log(error)
         <Container>
             <Row>
             {parks && parks.length > 0 ? 
-                parks.map((park: any) => {
+                parks.map((park: ParkItem) => {
                     console.log(park)
                     return (
                         <Card 
@@ -30,8 +42,8 @@ console.log(error)
                             name={park.fullName} 
                             description={park.description} 
                             img={park.images && park.images.length > 0 ? park.images[0] : null}
-                            coordinates={[park.latitude, park.longitude]} 
-                            // activities={park.activities} 
+                            coordinates={[{lat: park.latitude, long: park.longitude}]} 
+                            activities={park.activities.slice(0,2)} 
                             link={park.url}  
                         />
                     )}) : null}          
