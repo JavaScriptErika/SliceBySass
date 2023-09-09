d3168da8f190920bcf3f88b4137062b09a80f81d
JavaScriptErika <erikajritter@gmail.com>
2023-09-09 12:55:18 -0400

    create triangle with clip path

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import PizzaSlice from './components/PizzaSlice';
 
 const App = () => {
   return (
-    <></>
+    <PizzaSlice />
   );
 }
 
---
+++ src/components/PizzaSlice.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PizzaSlice = () => {
+    return (
+        <div className='pizza-base'>
+            <div className='crust'/>
+        </div>
+    )
+}
+
+export default PizzaSlice;
