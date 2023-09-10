411319bf86a8e51909dea9dca8f61844d23a7f05
JavaScriptErika <erikajritter@gmail.com>
2023-09-09 20:44:00 -0400

    add cheese and toppings, adjust slice

--- src/components/PizzaSlice.tsx
+++ src/components/PizzaSlice.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 const PizzaSlice = () => {
     return (
-        <div className='pizza-base'>
-            <div className='crust'/>
+        <div className='pizza-box'>
+            <div className='crust'></div>
+            <div className='base'>
+                <div className='toppings-container'></div>
+            </div>
+
+            <div className='cheese'></div>
         </div>
     )
 }
